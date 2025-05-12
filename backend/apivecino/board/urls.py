274c8b763ab4eea851d948838a22c1a8e3d88da2
Beatriz Sobrino Paredes <beatriz.sobrino.paredes@gmail.com>
2025-05-12 21:21:56 +0200

    clean board table (#24)
    Co-authored-by: Beatriz Sobrino Paredes <beatriz.sobrino@eventbrite.com>

--- backend/apivecino/board/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import BoardViewSet
-router = DefaultRouter()
-router.register(r'', BoardViewSet)
-urlpatterns = [
-    path('', include(router.urls)),
-]
