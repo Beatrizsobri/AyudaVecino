c87c4fb51c464581ffa0985ae87625424af15c67
Beatriz Sobrino Paredes <beatriz.sobrino.paredes@gmail.com>
2025-05-13 21:41:45 +0200

    Add contact details (#29)
    Co-authored-by: Beatriz Sobrino Paredes <beatriz.sobrino@eventbrite.com>

--- frontend/my-app/src/components/Navbar/NavbarUnlog.tsx
+++ frontend/my-app/src/components/Navbar/NavbarUnlog.tsx
@@ -7,12 +7,11 @@ const NavbarUnlog = () => {
     <nav className="bg-white shadow-sm sticky top-0 z-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between h-16">
-                <div className="flex items-center">
-                    <div className="flex-shrink-0 flex items-center">
+                <div className="flex-shrink-0 flex items-center">
+                    <a href={ROUTES.HOME}>
                         <img src={require('../../assets/logocolortitle.png')} alt="Ayuda Vecino Logo" className="h-28 w-auto" />
-                    </div>
+                    </a>
                 </div>
-
                 <div className="flex items-center space-x-4">
                     <a href={ROUTES.SIGN_IN} className="px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-600 hover:bg-indigo-50 transition duration-150 btn-hover">
                         Entrar
